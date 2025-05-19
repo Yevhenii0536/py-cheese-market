26ea1eb414fcbd4c9cb40042fd27de4fbe389a1a
Yevhenii0536 <eugeniy0536@gmail.com>
2025-05-19 15:39:06 +0300

    removed database url to the .env

--- db/engine.py
+++ db/engine.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 
+from settings import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///db.sqlite3"
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
--- settings.py
+++ settings.py
@@ -1,10 +1,18 @@
 from pydantic.v1 import BaseSettings
+from dotenv import load_dotenv
+
+import os
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Cheeese market"
 
-    DATABASE_URL: str = ""
+    DATABASE_URL: str = DATABASE_URL or ""
 
     class Config:
         case_sensitive = True
